586b80f43c325941e9d7f987e8f09499d4386052
Skaihen <92516247+Skaihen@users.noreply.github.com>
2022-06-07 11:43:09 +0200

    cambios

---
+++ src/clases/config.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace configfile;
+
+interface config
+{
+    public function addVars($name, $value): bool;
+    public function removeVars(string $name): bool;
+    public function modifyVars(string $name, $value): bool;
+    public function readVars(string $name): string;
+}
